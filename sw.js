54f32d4b8e2c2f4f64ffcbfde2b55ff20fbb718e
RobertYeongsu <122523516+RobertYeongsu@users.noreply.github.com>
2026-09-02 16:42:11 +0900

    update delete func

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dietapp-cache-v13';
+const CACHE_NAME = 'dietapp-cache-v14';
 const CORE_ASSETS = ['./index.html', './manifest.json'];
 
 self.addEventListener('install', (event) => {
