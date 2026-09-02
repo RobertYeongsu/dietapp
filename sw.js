f76a0ebb6c8a8b65d95f97bd4ea1e80e92e73034
RobertYeongsu <122523516+RobertYeongsu@users.noreply.github.com>
2026-09-02 14:34:32 +0900

    deleted dummy data

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dietapp-cache-v2';
+const CACHE_NAME = 'dietapp-cache-v3';
 const CORE_ASSETS = ['./index.html', './manifest.json'];
 
 self.addEventListener('install', (event) => {
