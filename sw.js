ae6680f2fd1c647461a0902addc51dc1aebf729b
RobertYeongsu <122523516+RobertYeongsu@users.noreply.github.com>
2026-09-02 14:42:31 +0900

    update new button and add photo

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dietapp-cache-v3';
+const CACHE_NAME = 'dietapp-cache-v4';
 const CORE_ASSETS = ['./index.html', './manifest.json'];
 
 self.addEventListener('install', (event) => {
