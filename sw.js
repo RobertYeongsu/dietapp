41a36870a949c85d291502629b7dfa8b56e58122
RobertYeongsu <122523516+RobertYeongsu@users.noreply.github.com>
2026-09-02 15:14:11 +0900

    add api key with cloudflare

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dietapp-cache-v4';
+const CACHE_NAME = 'dietapp-cache-v5';
 const CORE_ASSETS = ['./index.html', './manifest.json'];
 
 self.addEventListener('install', (event) => {
