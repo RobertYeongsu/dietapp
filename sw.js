4f5f84c6f87f543c0487c8b02ad552b8e580064f
RobertYeongsu <122523516+RobertYeongsu@users.noreply.github.com>
2026-09-02 17:24:20 +0900

    reload set today

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dietapp-cache-v17';
+const CACHE_NAME = 'dietapp-cache-v18';
 const CORE_ASSETS = ['./index.html', './manifest.json'];
 
 self.addEventListener('install', (event) => {
