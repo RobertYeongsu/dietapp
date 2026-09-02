c2786786e7e066b506b2d907cd2ffda2bbcf6f18
RobertYeongsu <122523516+RobertYeongsu@users.noreply.github.com>
2026-09-02 17:18:47 +0900

    change ai model

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dietapp-cache-v16';
+const CACHE_NAME = 'dietapp-cache-v17';
 const CORE_ASSETS = ['./index.html', './manifest.json'];
 
 self.addEventListener('install', (event) => {
