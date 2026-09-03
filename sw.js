ea32dd4fbb6fd197eb7fc0c08e0a92dbe388b812
RobertYeongsu <122523516+RobertYeongsu@users.noreply.github.com>
2026-09-03 12:08:25 +0900

    update rescan button

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dietapp-cache-v20';
+const CACHE_NAME = 'dietapp-cache-v21';
 const CORE_ASSETS = ['./index.html', './manifest.json'];
 
 self.addEventListener('install', (event) => {
