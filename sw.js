3a46a27cb562c117a195ad71cb09693ef160bfb5
RobertYeongsu <122523516+RobertYeongsu@users.noreply.github.com>
2026-09-03 12:20:10 +0900

    fixed caemra if home icon

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dietapp-cache-v22';
+const CACHE_NAME = 'dietapp-cache-v24';
 const CORE_ASSETS = ['./index.html', './manifest.json'];
 
 self.addEventListener('install', (event) => {
